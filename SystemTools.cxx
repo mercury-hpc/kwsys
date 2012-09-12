924466f53428ad1b423411fccb34ba6729403855
Sean McBride <sean@rogue-research.com>
2012-09-12 14:19:12 -0400

    SystemTools: Fix accidental bitwise instead of logical OR
    Change-Id: Ia49de4dfa022f9f284733e8b42c22c987b145bde

--- SystemTools.cxx
+++ SystemTools.cxx
@@ -1669,7 +1669,7 @@ kwsys_stl::string SystemTools::EscapeChars(
   kwsys_stl::string n;
   if (str)
     {
-    if (!chars_to_escape | !*chars_to_escape)
+    if (!chars_to_escape || !*chars_to_escape)
       {
       n.append(str);
       }
