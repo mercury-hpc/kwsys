5690d711ba3ab6f11057341ebee3e282c0175cb3
Rolf Eike Beer <eike@sf-mail.de>
2012-11-30 17:36:23 +0100

    SystemInformation: fix value extraction from /proc/cpuinfo
    If the given key (e.g. "cpu") is matched at the beginning of any line the value
    was returned regardless if that line would be the correct match (e.g.
    "cpu family" is not the correct line). Make sure between the key and the colon
    are only spaces and tabs.
    
    Change-Id: I8955add3635a0afb73a9f07f7a0bcf600b7c7015

--- SystemInformation.cxx
+++ SystemInformation.cxx
@@ -3002,6 +3002,16 @@ kwsys_stl::string SystemInformationImplementation::ExtractValueFromCpuInfoFile(k
     size_t pos2 = buffer.find("\n",pos);
     if(pos!=buffer.npos && pos2!=buffer.npos)
       {
+      // It may happen that the beginning matches, but this is still not the requested key.
+      // An example is looking for "cpu" when "cpu family" comes first. So we check that
+      // we have only spaces from here to pos, otherwise we search again.
+      for(size_t i=this->CurrentPositionInFile+strlen(word); i < pos; ++i)
+        {
+        if(buffer[i] != ' ' && buffer[i] != '\t')
+          {
+          return this->ExtractValueFromCpuInfoFile(buffer, word, pos2);
+          }
+        }
       return buffer.substr(pos+2,pos2-pos-2);
       }
     }
