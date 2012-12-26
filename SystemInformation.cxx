ea5612ed516bfacb66ed74f6772418544f54b978
Rolf Eike Beer <eike@sf-mail.de>
2012-12-26 15:00:54 +0100

    SystemInformation: count memory with _SC_PAGESIZE and _SC_PHYS_PAGES
    If there is no other way known for this particular system try this out.
    
    Change-Id: Ib06d5c289d7ca625070ad7482bd575ad5e549f0c

--- SystemInformation.cxx
+++ SystemInformation.cxx
@@ -3426,6 +3426,39 @@ int SystemInformationImplementation::QueryMemory()
     }
   return 0;
 #else
+
+#if defined(_SC_PHYS_PAGES) && defined(_SC_PAGESIZE)
+  // Assume the mmap() granularity as returned by _SC_PAGESIZE is also
+  // the system page size. The only known system where this isn't true
+  // is Cygwin.
+  long p = sysconf(_SC_PHYS_PAGES);
+  long m = sysconf(_SC_PAGESIZE);
+
+  if (p < 0 || m < 0)
+    {
+    return 0;
+    }
+
+  // assume pagesize is a power of 2 and smaller 1 MiB
+  size_t pagediv = (1024 * 1024 / m);
+
+  this->TotalPhysicalMemory = p;
+  this->TotalPhysicalMemory /= pagediv;
+
+#if defined(_SC_AVPHYS_PAGES)
+  p = sysconf(_SC_AVPHYS_PAGES);
+  if (p < 0)
+    {
+    return 0;
+    }
+
+  this->AvailablePhysicalMemory = p;
+  this->AvailablePhysicalMemory /= pagediv;
+#endif
+
+  return 1;
+#endif
+
   return 0;
 #endif
 }
