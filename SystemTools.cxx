cb5f835f48209157701243093378b739fed9f147
Brad King <brad.king@kitware.com>
2013-01-02 11:45:48 -0500

    SystemTools: Fix MakeDirectory with colon in path
    Remove logic that skips all slashes before the first ':'.  This does not
    even make sense for Windows drive letter handling.  While at it, remove
    no-op logic that appears to try to remove trailing slashes.
    
    Change-Id: Ib8ed75324ee0149c7899e52ec4a5bf2d0a19d17a

--- SystemTools.cxx
+++ SystemTools.cxx
@@ -622,25 +622,13 @@ bool SystemTools::MakeDirectory(const char* path)
     }
   SystemTools::ConvertToUnixSlashes(dir);
 
-  kwsys_stl::string::size_type pos = dir.find(':');
-  if(pos == kwsys_stl::string::npos)
-    {
-    pos = 0;
-    }
+  kwsys_stl::string::size_type pos = 0;
   kwsys_stl::string topdir;
   while((pos = dir.find('/', pos)) != kwsys_stl::string::npos)
     {
     topdir = dir.substr(0, pos);
     Mkdir(topdir.c_str());
     pos++;
-    }
-  if(dir[dir.size()-1] == '/')
-    {
-    topdir = dir.substr(0, dir.size());
-    }
-  else
-    {
-    topdir = dir;
     }
   if(Mkdir(topdir.c_str()) != 0)
     {
