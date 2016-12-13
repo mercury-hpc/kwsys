41a7fe5b0350548ee04acbdee0e60c9df47e3c79
Brad King <brad.king@kitware.com>
2016-12-13 14:23:54 -0500

    Add pre-commit hook to check whether SetupForDevelopment must re-run
    Add a version number to the `SetupForDevelopment.sh` script and use
    a pre-commit hook to check when it changes.

--- SetupForDevelopment.sh
+++ SetupForDevelopment.sh
@@ -10,3 +10,7 @@ GitSetup/tips
 # Rebase master by default
 git config rebase.stat true
 git config branch.master.rebase true
+
+# Record the version of this setup so Scripts/pre-commit can check it.
+SetupForDevelopment_VERSION=1
+git config hooks.SetupForDevelopment ${SetupForDevelopment_VERSION}
