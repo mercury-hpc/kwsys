17a1200c70d67f0646fef6e1bc3abf0ac61b124d
Brad King <brad.king@kitware.com>
2016-12-13 14:06:13 -0500

    Convert developer setup scripts away from Gerrit Code Review
    KWSys development is moving away from the Gerrit Code Review system.
    Disable portions of `SetupForDevelopment.sh` related to Gerrit.

--- SetupForDevelopment.sh
+++ SetupForDevelopment.sh
@@ -3,14 +3,18 @@
 cd "${BASH_SOURCE%/*}" &&
 GitSetup/setup-user && echo &&
 GitSetup/setup-hooks && echo &&
-GitSetup/setup-gerrit && echo &&
 GitSetup/setup-aliases && echo &&
+GitSetup/setup-upstream && echo &&
 GitSetup/tips
 
 # Rebase master by default
 git config rebase.stat true
 git config branch.master.rebase true
 
+# Disable Gerrit hook explicitly so the commit-msg hook will
+# not complain even if some gerrit remotes are still configured.
+git config hooks.GerritId false
+
 # Record the version of this setup so Scripts/pre-commit can check it.
-SetupForDevelopment_VERSION=1
+SetupForDevelopment_VERSION=2
 git config hooks.SetupForDevelopment ${SetupForDevelopment_VERSION}
