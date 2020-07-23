mercury-hpc/kwsys

commit 45db09371ec74e2c223a42e840386e9cee1d694b

SystemTools: use PathExists where appropriate
The `Touch` method should care about existence and directory creation
should also be guarded against unreadable paths as well. In addition,
fix a path existence check which should have been updated before.

SystemTools.cxx
@@ -763,7 +763,7 @@ bool SystemTools::MakeDirectory(const char* path)
 
 bool SystemTools::MakeDirectory(const std::string& path)
 {
-  if (SystemTools::FileExists(path)) {
+  if (SystemTools::PathExists(path)) {
     return SystemTools::FileIsDirectory(path);
   }
   if (path.empty()) {
@@ -1280,7 +1280,7 @@ bool SystemTools::PathCygwinToWin32(const char* path, char* win32_path)
 
 bool SystemTools::Touch(const std::string& filename, bool create)
 {
-  if (!SystemTools::FileExists(filename)) {
+  if (!SystemTools::PathExists(filename)) {
     if (create) {
       FILE* file = Fopen(filename, "a+b");
       if (file) {
@@ -4389,10 +4389,7 @@ bool SystemTools::SetPermissions(const char* file, mode_t mode,
 bool SystemTools::SetPermissions(const std::string& file, mode_t mode,
                                  bool honor_umask)
 {
-  // TEMPORARY / TODO:  After FileExists calls lstat() instead of
-  // access(), change this call to FileExists instead of
-  // TestFileAccess so that we don't follow symlinks.
-  if (!SystemTools::TestFileAccess(file, TEST_FILE_OK)) {
+  if (!SystemTools::PathExists(file)) {
     return false;
   }
   if (honor_umask) {

commit 22216d56140a7c1b29c4f2ea460a5dfc09a4c706

SystemTools: Fix Touch() on broken symlink when create==false
The `touch -c` command-line tool does nothing when given a broken
symlink.  Fix `SystemTools::Touch` to do the same.

This was broken by commit 45db09371 (SystemTools: use PathExists where
appropriate, 2017-01-10).

SystemTools.cxx
@@ -1453,7 +1453,7 @@ bool SystemTools::PathCygwinToWin32(const char* path, char* win32_path)
 
 bool SystemTools::Touch(const std::string& filename, bool create)
 {
-  if (!SystemTools::PathExists(filename)) {
+  if (!SystemTools::FileExists(filename)) {
     if (create) {
       FILE* file = Fopen(filename, "a+b");
       if (file) {

testSystemTools.cxx
@@ -422,6 +422,28 @@ static bool CheckFileOperations()
     res = false;
   }
 
+#if !defined(_WIN32)
+  std::string const testBadSymlink(testNewDir + "/badSymlink.txt");
+  std::string const testBadSymlinkTgt(testNewDir + "/missing/symlinkTgt.txt");
+  if (!kwsys::SystemTools::CreateSymlink(testBadSymlinkTgt, testBadSymlink)) {
+    std::cerr << "Problem with CreateSymlink for: " << testBadSymlink << " -> "
+              << testBadSymlinkTgt << std::endl;
+    res = false;
+  }
+
+  if (!kwsys::SystemTools::Touch(testBadSymlink, false)) {
+    std::cerr << "Problem with Touch (no create) for: " << testBadSymlink
+              << std::endl;
+    res = false;
+  }
+#endif
+
+  if (!kwsys::SystemTools::Touch(testNewDir, false)) {
+    std::cerr << "Problem with Touch (no create) for: " << testNewDir
+              << std::endl;
+    res = false;
+  }
+
   kwsys::SystemTools::Touch(testNewFile, true);
   if (!kwsys::SystemTools::RemoveADirectory(testNewDir)) {
     std::cerr << "Problem with RemoveADirectory for: " << testNewDir